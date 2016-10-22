b5375c30f32b925c755841da79799e5de5d30ed6
Ruslan Gainanov <gromrx1@gmail.com>
2016-10-22 17:58:22 +0500

    Add a pushed interval and buffer of write data

--- udp_listener/dhcp_listener.rb
+++ udp_listener/dhcp_listener.rb
@@ -6,13 +6,17 @@ require 'date'
 require 'pp'
 
 INFLUX_HOST = "localhost"
-DHCP_LISTEN_PORT = 5242
-DHCP_LISTEN_ADDR = "0.0.0.0"
+LISTEN_PORT = 5242
+LISTEN_ADDR = "0.0.0.0"
 TIME_ZONE = 5
+COUNT_STORED_ELEMENTS = 100
+TIME_RANGE=3*60 #pushed the time range, in seconds
 
+@stored_data=[]
 @influxdb = InfluxDB::Client.new host: INFLUX_HOST, 
   database: "telegraf", 
   time_precision: 's'
+@last_pushed_time=0
 
 def fix_data( h )
   h.delete("@version")
@@ -34,10 +38,16 @@ def fix_data( h )
   h.store("timestamp", (dt.to_time-60*60*(TIME_ZONE)).to_i)
 end
 
+def push_data()
+  if(@stored_data.length >= COUNT_STORED_ELEMENTS || (Time.now.to_i - @last_pushed_time > TIME_RANGE))
+    @influxdb.write_points(@stored_data)
+    @stored_data.clear
+  end
+end
+
 def save_data( h )
-  data = []
   ts = h.delete("timestamp")
-  data << {
+  @stored_data << {
     series: "dhcp_real",
     tags: { id: h["id"], 
             description: h["description"],  
@@ -48,27 +58,23 @@ def save_data( h )
     timestamp: ts
   }
   # puts JSON.pretty_generate(h)
-  data << {
+  @stored_data << {
     series: "dhcp_all",
     tags: h,
     values: { z: 0 },
     timestamp: ts
   }
-  @influxdb.write_points(data)
 end
 
 server = UDPSocket.new
-server.bind(DHCP_LISTEN_ADDR, DHCP_LISTEN_PORT)
+server.bind(LISTEN_ADDR, LISTEN_PORT)
 puts "start"
-data = []
-i = 0
 loop do
   text, sender = server.recvfrom(65536)  
   # text, sender = server.recvfrom_nonblock(65536)  
     #=> ["aaa", ["AF_INET", 33302, "localhost.localdomain", "127.0.0.1"]]
   # puts "#{text}:\n  #{sender}"
   # puts JSON.parse(text)
-  # data << JSON.parse(text)
   data = JSON.parse(text)
   fix_data(data)
   save_data(data)
