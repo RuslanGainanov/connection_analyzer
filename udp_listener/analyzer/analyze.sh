3e8f7c7adbab565d043919375ab5fb0b42c62b95
Ruslan Gainanov <gromrx1@gmail.com>
2016-10-28 17:23:26 +0500

    Add the full path for ruby

--- udp_listener/analyzer/analyze.sh
+++ udp_listener/analyzer/analyze.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ruby /var/udp_listener/analyzer/connections_users.rb >> /var/log/udp_listener/analyzer.log 2>&1
+/usr/local/bin/ruby /var/udp_listener/analyzer/connections_users.rb >> /var/log/udp_listener/analyzer.log 2>&1
