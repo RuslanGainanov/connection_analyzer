0d5b3bf3a7bd987b3c9b9a08a4e0d9f80d46f0f6
Ruslan Gainanov <gromrx1@gmail.com>
2016-11-03 20:01:22 +0500

    Fix file names.

--- analyze.sh
+++ analyze.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/ruby /var/udp_listener/analyzer/connections_users.rb >> /var/log/udp_listener/analyzer.log 2>&1
+/usr/local/bin/ruby /var/connection_analyzer/connections_users.rb >> /var/log/connection_analyzer/analyzer.log 2>&1
--- run_task.sh
+++ run_task.sh
@@ -1,5 +1,5 @@
-chmod +x /var/udp_listener/analyzer/analyze.sh
-crontab -l > /var/udp_listener/analyzer/backup_cron_tasks
+chmod +x /var/connection_analyzer/analyze.sh
+crontab -l > /var/connection_analyzer/backup_cron_tasks
 crontab -r
-/var/udp_listener/analyzer/analyze.sh
-crontab /var/udp_listener/analyzer/cron-file.txt
+/var/connection_analyzer/analyze.sh
+crontab /var/connection_analyzer/cron-file.txt
