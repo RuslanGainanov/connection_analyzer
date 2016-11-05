1a81497ceb951faa57da44228e4d35a8c366703d
Ruslan Gainanov <gromrx1@gmail.com>
2016-11-05 20:50:19 +0500

    Add syslog to scripts.

--- udp_listener/run_listeners.sh
+++ udp_listener/run_listeners.sh
@@ -3,3 +3,4 @@ su -s /bin/sh - ruby_runner -c /var/udp_listener/run_dhcp.sh &
 
 su -s /bin/sh - ruby_runner -c /var/udp_listener/run_nps.sh &
 
+su -s /bin/sh - ruby_runner -c /var/udp_listener/run_syslog.sh &
